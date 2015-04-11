6b2e3d75cbc72638241ec4d7887c06d33173e4b2
Subhojit Paul <subhojitpaul21@gmail.com>
2015-04-12 00:10:30 +0530

    Unnecessary files removed

--- public/scripts/loader.js
+++
@@ -1,9 +0,0 @@
-$('document').ready(function() {
-  $('document').ajaxStart(function() {
-    $('.todoList .progress').removeClass('hidden');
-    $('.todoList .list').addClass('hidden');
-  }).ajaxStop(function() {
-    $('.todoList .progress').addClass('hidden');
-    $('.todoList .list').removeClass('hidden');
-  });
-});
